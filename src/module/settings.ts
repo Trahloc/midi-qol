07349fd14089f02ece68f67f2a9650b036fa9384
Tim Posney <tposney@gmail.com>
2023-09-08 12:57:06 +1000

    11.0.15.3

--- src/module/settings.ts
+++ src/module/settings.ts
@@ -383,7 +383,7 @@ export let fetchParams = () => {
   if (!configSettings.optionalRules.checkRange) configSettings.optionalRules.checkRange = "none";
   if (!configSettings.optionalRules.invisAdvantage) configSettings.optionalRules.invisAdvantage = "none";
   if (configSettings.optionalRules.invisAdvantage === true) configSettings.optionalRules.invisAdvantage = "RAW";
-  if (configSettings.confirmAttackDamage == undefined) configSettings.optionalRules.confirmAttackDamage = "none";
+  if (typeof configSettings.confirmAttackDamage !== "string") configSettings.optionalRules.confirmAttackDamage = "none";
   if (typeof configSettings.requireMagical !== "string" && configSettings.requireMagical !== true) configSettings.requireMagical = "off";
   if (typeof configSettings.requireMagical !== "string" && configSettings.requireMagical === true) configSettings.requireMagical = "nonspell";
 
